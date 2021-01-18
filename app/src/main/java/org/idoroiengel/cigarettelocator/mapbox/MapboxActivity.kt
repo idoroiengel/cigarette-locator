6e9b5de34eb85762d7fb380cd53e19666497354f
idoroiengel <idoroiengel@gmail.com>
2021-01-18 12:23:08 +0200

    - first working implementation of LocationComponent in mapbox sdk

--- app/src/main/java/org/idoroiengel/cigarettelocator/mapbox/MapboxActivity.kt
+++ app/src/main/java/org/idoroiengel/cigarettelocator/mapbox/MapboxActivity.kt
@@ -1,16 +1,35 @@
 package org.idoroiengel.cigarettelocator.mapbox
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.mapbox.android.core.location.*
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
 import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
 import org.idoroiengel.cigarettelocator.R
+import java.lang.ref.WeakReference
 
-class MapboxActivity: AppCompatActivity() {
 
+class MapboxActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListener {
+
+    private var locationEngine: LocationEngine? = null
+    private var permissionsManager: PermissionsManager? = null
     private var mapView: MapView? = null
+    private var mapboxMap: MapboxMap? = null
+    private var callback: MapboxActivityLocationCallback = MapboxActivityLocationCallback(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +39,7 @@ class MapboxActivity: AppCompatActivity() {
 
         mapView = findViewById(R.id.map_view)
         mapView?.onCreate(savedInstanceState)
-        mapView?.getMapAsync { mapboxMap ->
-            mapboxMap.setStyle(Style.MAPBOX_STREETS)
-        }
+        mapView?.getMapAsync(this)
     }
 
     override fun onStart() {
@@ -58,5 +75,128 @@ class MapboxActivity: AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         mapView?.onDestroy()
+    }
+
+    override fun onMapReady(mapboxMap: MapboxMap) {
+        Log.d("MapboxActivity", "onMapReady")
+        this.mapboxMap = mapboxMap
+        mapboxMap.setStyle(
+            Style.MAPBOX_STREETS,
+            Style.OnStyleLoaded { style -> enableLocationComponent(style) })
+
+    }
+
+    private fun enableLocationComponent(style: Style) {
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            val locationComponent = mapboxMap!!.locationComponent
+            val locationComponentActivationOptions =
+                LocationComponentActivationOptions.builder(this, style)
+                    .useDefaultLocationEngine(false)
+                    .build()
+            locationComponent.activateLocationComponent(locationComponentActivationOptions)
+            locationComponent.setLocationComponentEnabled(true)
+            locationComponent.setCameraMode(CameraMode.TRACKING)
+            locationComponent.setRenderMode(RenderMode.COMPASS)
+            initLocationEngine()
+        } else {
+            permissionsManager = PermissionsManager(this)
+            permissionsManager?.requestLocationPermissions(this)
+        }
+
+    }
+
+    private fun initLocationEngine() {
+        locationEngine = LocationEngineProvider.getBestLocationEngine(this)
+        val request = LocationEngineRequest.Builder(1000)
+            .setPriority(LocationEngineRequest.PRIORITY_HIGH_ACCURACY)
+            .setMaxWaitTime(3000).build()
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        locationEngine?.requestLocationUpdates(request, callback, Looper.getMainLooper())
+        locationEngine?.getLastLocation(callback!!)
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: List<String?>?) {
+        Toast.makeText(this, "we need your location permission", Toast.LENGTH_LONG)
+            .show()
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted) {
+            if (mapboxMap!!.style != null) {
+                enableLocationComponent(mapboxMap!!.style!!)
+            }
+        } else {
+            Toast.makeText(this, "We have your location permission", Toast.LENGTH_LONG)
+                .show()
+            finish()
+        }
+    }
+
+    private class MapboxActivityLocationCallback internal constructor(activity: MapboxActivity?) :
+        LocationEngineCallback<LocationEngineResult> {
+        private val activityWeakReference: WeakReference<MapboxActivity>
+
+        /**
+         * The LocationEngineCallback interface's method which fires when the device's location has changed.
+         *
+         * @param result the LocationEngineResult object which has the last known location within it.
+         */
+        override fun onSuccess(result: LocationEngineResult) {
+            val activity: MapboxActivity = activityWeakReference.get()!!
+            if (activity != null) {
+                val location = result.lastLocation ?: return
+
+// Create a Toast which displays the new location's coordinates
+                Toast.makeText(
+                    activity, String.format(
+                        "This is your new location", result.lastLocation!!
+                            .latitude.toString(), result.lastLocation!!.longitude.toString()
+                    ),
+                    Toast.LENGTH_SHORT
+                ).show()
+
+// Pass the new location to the Maps SDK's LocationComponent
+                if (activity.mapboxMap != null && result.lastLocation != null) {
+                    activity.mapboxMap!!.getLocationComponent()
+                        .forceLocationUpdate(result.lastLocation)
+                }
+            }
+        }
+
+        /**
+         * The LocationEngineCallback interface's method which fires when the device's location can not be captured
+         *
+         * @param exception the exception message
+         */
+        override fun onFailure(exception: Exception) {
+            Log.d("LocationChangeActivity", exception.localizedMessage)
+            val activity: MapboxActivity? = activityWeakReference.get()
+            if (activity != null) {
+                Toast.makeText(
+                    activity, exception.localizedMessage,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        init {
+            activityWeakReference = WeakReference(activity)
+        }
     }
 }
